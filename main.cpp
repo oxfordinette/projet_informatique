48e36284c031cd90b1d60c4115705bafdf1749fb
oxfordinette <sandrabaraket@gmail.com>
2018-04-06 14:28:17 +0200

    programme principal

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
+#include "grman/grman.h"
 #include <iostream>
 
-using namespace std;
+#include "graph.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+    int idx=2;
+
+    /// Un exemple de graphe
+    Graph g;
+    g.charger_graph();
+    g.supprimer_arrete(idx);
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update();
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+        g.creerSommet();
+        g.creerArrete();
+        g.supprimerSommet();
+
+    }
+
+    grman::fermer_allegro();
+    g.sauver_graph();
+
     return 0;
 }
+END_OF_MAIN();
+
+
